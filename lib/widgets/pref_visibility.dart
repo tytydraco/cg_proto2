97908cfda183cbf6c2ab723c5b4a295454f967d6
Tyler Nijmeh <tylernij@gmail.com>
2022-05-19 15:33:38 -0700

    Abstract visiblity widget
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ lib/widgets/pref_visibility.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// A Visibility widget based on a SharedPreference key
+class PrefVisibility extends StatefulWidget {
+  final String prefKey;
+  final Widget child;
+
+  const PrefVisibility({
+    Key? key,
+    required this.prefKey,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  State<PrefVisibility> createState() => _PrefVisibilityState();
+}
+
+class _PrefVisibilityState extends State<PrefVisibility> {
+  Future<bool> isVisible() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    return sharedPrefs.getBool(widget.prefKey) ?? true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: isVisible(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final isVisible = snapshot.data as bool;
+          return Visibility(
+            visible: isVisible,
+            child: widget.child,
+          );
+        } else if (snapshot.hasError) {
+          return const Icon(Icons.warning);
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
