1899bd593b566f2a8fd6e7170b1b3df74776686c
Tyler Nijmeh <tylernij@gmail.com>
2022-05-20 13:07:25 -0700

    Beautiful charts
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/widgets/site_weather_chart.dart
+++ lib/widgets/site_weather_chart.dart
@@ -2,15 +2,14 @@ import 'package:cg_proto2/models/site_model.dart';
 import 'package:cg_proto2/models/site_weather_model.dart';
 import 'package:cg_proto2/remote/remote_database.dart';
 import 'package:cg_proto2/widgets/pref_visibility.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SiteWeatherChart extends StatefulWidget {
   final String id;
   final String title;
   final SiteModel site;
-  final num Function(SiteWeatherModel) yFn;
+  final double Function(SiteWeatherModel) yFn;
   final List<SiteWeatherModel> Function(List<SiteWeatherModel>)? filterFn;
 
   const SiteWeatherChart({
@@ -39,6 +38,10 @@ class _SiteWeatherChartState extends State<SiteWeatherChart> {
     return siteWeathers;
   }
 
+  Widget leftTitleWidget(double value, TitleMeta? _) {
+    return Text(value.toInt().toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return PrefVisibility(
@@ -48,32 +51,61 @@ class _SiteWeatherChartState extends State<SiteWeatherChart> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final siteWeathers = snapshot.data as List<SiteWeatherModel>;
-            final List<charts.Series<SiteWeatherModel, num>> series = [
-              charts.Series(
-                id: widget.id,
-                data: siteWeathers,
-                domainFn: (SiteWeatherModel siteWeather, int? index) => index!,
-                measureFn: (SiteWeatherModel siteWeather, _) => widget.yFn(siteWeather),
-              )
-            ];
 
-            return Card(
-              margin: const EdgeInsets.only(left: 8, right: 8, top: 8),
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Text(widget.title),
-                    SizedBox(
-                      height: 200,
-                      child: charts.LineChart(
-                        series,
-                        animate: true,
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 18, right: 18),
+                    child: Text(
+                      widget.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold
                       ),
                     ),
-                  ],
-                )
-              ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: AspectRatio(
+                    aspectRatio: 2.5,
+                    child: LineChart(
+                      LineChartData(
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: siteWeathers.asMap().entries.map((entry) {
+                              final index = entry.key;
+                              final siteWeather = entry.value;
+                              return FlSpot(index.toDouble(), widget.yFn(siteWeather));
+                            }).toList(),
+                            dotData: FlDotData(show: false),
+                            isCurved: true,
+                            isStrokeCapRound: true,
+                            barWidth: 3,
+                            belowBarData: BarAreaData(show: false),
+                            color: Colors.black,
+                          ),
+                        ],
+                        titlesData: FlTitlesData(
+                          topTitles: AxisTitles(),
+                          leftTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: leftTitleWidget,
+                              reservedSize: 40,
+                            ),
+                          ),
+                          bottomTitles: AxisTitles(),
+                          rightTitles: AxisTitles(),
+                        ),
+                        borderData: FlBorderData(show: false),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             );
           } else if (snapshot.hasError) {
             return const Text('Failed to fetch weather data!');
