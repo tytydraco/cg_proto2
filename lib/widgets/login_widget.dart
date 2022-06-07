083f6f65c22eae65c3c8bda8e174a4cc654fae9e
Tyler Nijmeh <tylernij@gmail.com>
2022-06-07 15:17:03 -0700

    Save successful credentials
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/widgets/login_widget.dart
+++ lib/widgets/login_widget.dart
@@ -2,6 +2,7 @@ import 'package:cg_proto2/models/credential_model.dart';
 import 'package:cg_proto2/remote/remote_auth.dart';
 import 'package:cg_proto2/widgets/login_save_checkbox.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginWidget extends StatefulWidget {
   final Function onSuccess;
@@ -24,12 +25,46 @@ class _LoginWidgetState extends State<LoginWidget> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  void attemptLogin() async {
+  Future<bool> shouldSaveCredentials() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    return sharedPrefs.getBool('save_credentials') ?? false;
+  }
+
+  Future putSavedCredentials(CredentialModel credentials) async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    sharedPrefs.setString('saved_username', credentials.username);
+    sharedPrefs.setString('saved_password', credentials.password);
+  }
+
+  Future<CredentialModel> getSavedCredentials() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final savedUsername = sharedPrefs.getString('saved_username') ?? '';
+    final savedPassword = sharedPrefs.getString('saved_password') ?? '';
+    return CredentialModel(savedUsername, savedPassword);
+  }
+
+  Future updateCredentials() async {
+    final saveCredentials = await shouldSaveCredentials();
+    if (saveCredentials) {
+      final savedCredentials = await getSavedCredentials();
+      usernameController.text = savedCredentials.username;
+      passwordController.text = savedCredentials.password;
+
+      username = savedCredentials.username;
+      password = savedCredentials.password;
+    }
+  }
+
+  Future attemptLogin() async {
     final remoteAuth = RemoteAuth();
     final credential = CredentialModel(username, password);
     final success = await remoteAuth.checkCredentials(credential);
 
     if (success) {
+      final saveCredentials = await shouldSaveCredentials();
+      if (saveCredentials) {
+        await putSavedCredentials(credential);
+      }
       widget.onSuccess();
     } else {
       passwordController.clear();
@@ -42,6 +77,8 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
+    updateCredentials();
+
     return Column(
       children: [
         TextFormField(
