32439db1604b23a6a90655b27970eadf2601c876
Tyler Nijmeh <tylernij@gmail.com>
2022-06-09 14:20:58 -0700

    Small added new line
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,4 +1,5 @@
 /// The list of selectable intervals (in days) that our charts can display.
 const chartIntervals = [7, 14, 21, 28];
+
 /// The default value to use (must be in [chartIntervals]).
 const chartIntervalDefault = 28;
